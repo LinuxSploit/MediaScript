7769c902c66333b78db2f44bf72363d1a2992eec
Bilal Hameed <68427058+LinuxSploit@users.noreply.github.com>
2023-05-02 11:37:26 +0000

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-#export C_INCLUDE_PATH=/home/linuxsploit/Desktop/fiverr_projects/MediaScript/cport/
-#export LIBRARY_PATH=/home/linuxsploit/Desktop/fiverr_projects/MediaScript/cport/
-
 export C_INCLUDE_PATH=./whisper.cpp/
 export LIBRARY_PATH=./whisper.cpp/
