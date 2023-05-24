2971a0ec21c4a444d636a6885e7d9455edf857e9
Bilal Hameed <68427058+LinuxSploit@users.noreply.github.com>
2023-05-24 08:21:54 +0000

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export C_INCLUDE_PATH=./whisper.cpp/
-export LIBRARY_PATH=./whisper.cpp/
-export FYNE_FONT=./fonts/Arial\ Unicode\ MS.ttf
+export C_INCLUDE_PATH=$(pwd)./whisper.cpp/
+export LIBRARY_PATH=$(pwd)./whisper.cpp/
+export FYNE_FONT=$(pwd)./fonts/Arial\ Unicode\ MS.ttf
